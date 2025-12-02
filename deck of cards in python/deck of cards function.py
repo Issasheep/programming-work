39ab40001b09147f6b6368f90eb817fec39cc9a0
Issasheep <111997535+Issasheep@users.noreply.github.com>
2025-12-02 16:39:41 +1000

    small rework

--- deck of cards in python/deck of cards function.py
+++ deck of cards in python/deck of cards function.py
@@ -4,76 +4,28 @@ bet = 0
 full_deck = []
 
 class Card:
-    def __init__(self, suit, rank):
-        self.suit = suit     # stored on the instance
+    def __init__(self, suit, rank, value):
+        self.suit = suit
         self.rank = rank
+        self.value = value
+
     def __str__(self):
-        return f"{self.rank} of {self.suit}"
-    def __repr__(self):
-        return self.__str__()
+        return f"{self.rank} of {self.suit} (Value: {self.value})"
 
 class face_card:
     def __init__(self):
         self.suit = 'Face Cards'
-        self.values = {
-            'Jack': 10, 'Queen': 10, 'King': 10
-        }
-    def __str__(self):
-        return f"{self.suit}: {self.values}"
-    
-"""
-
-class clubs:
-    def __init__(self):
-        self.suit = 'Clubs'
-        self.values = {
-            'Ace': 11,    # Ace can be 1 or 11 in Blackjack
-            '2': 2, '3': 3, '4': 4, '5': 5,
-            '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
-            'Jack': 10, 'Queen': 10, 'King': 10
-        }
+        self.value = 10
+        self.values = {'Jack', 'Queen', 'King'}
     def __str__(self):
         return f"{self.suit}: {self.values}"
 
-
-class diamonds:
-    def __init__(self):
-        self.suit = 'Diamonds'
-        self.values = {
-            'Ace': 11,    # Ace can be 1 or 11 in Blackjack
-            '2': 2, '3': 3, '4': 4, '5': 5,
-            '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
-            'Jack': 10, 'Queen': 10, 'King': 10
-        }
-    def __str__(self):
-        return f"{self.suit}: {self.values}"
+class suits: 
+    def __init__(self, cards):
+        self.cards = cards
+        self.suits = ['Clubs', 'Hearts', 'Diamonds', 'Spades']
 
 
-
-class hearts:
-    def __init__(self):
-        self.suit = 'Hearts'
-        self.values = {
-            'Ace': 11,    # Ace can be 1 or 11 in Blackjack
-            '2': 2, '3': 3, '4': 4, '5': 5,
-            '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
-            'Jack': 10, 'Queen': 10, 'King': 10
-        }
-    def __str__(self):
-        return f"{self.suit}: {self.values}"
-
-class spades:
-    def __init__(self):
-        self.suit = 'Spades'
-        self.values = {
-            'Ace': 11,    # Ace can be 1 or 11 in Blackjack
-            '2': 2, '3': 3, '4': 4, '5': 5,
-            '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
-            'Jack': 10, 'Queen': 10, 'King': 10
-        }
-    def __str__(self):
-        return f"{self.suit}: {self.values}"
-"""
 
 def get_cards(self):
     return self.cards
@@ -83,25 +35,32 @@ def get_cards(self):
 def deck_of_cards():
     deck = []
     suits = ['Clubs', 'Hearts', 'Diamonds', 'Spades']
-    values = {
-        'Ace': 11,
-        '2': 2, '3': 3, '4': 4, '5': 5,
-        '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
-        'Jack': 10, 'Queen': 10, 'King': 10
+    ranks_values = {
+        '2': 2, '3': 3, '4': 4, '5': 5, '6': 6,
+        '7': 7, '8': 8, '9': 9, '10': 10,
+        'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11
     }
-    
+
     for suit in suits:
-        for rank, value in values.items():
-            card = Card(suit, rank)
-            deck.append(card)
+        for rank, value in ranks_values.items():
+            deck.append(Card(suit, rank, value))
     
     return deck
+
+
 
 def shuffle_deck(deck_of_cards):
     import random
     random.shuffle(deck_of_cards)
     return deck_of_cards
 
+deckes = shuffle_deck(deck_of_cards())
+for i in range (52):
+    print (deckes[0].value)
+
+
+
+"""
 #full_deck = shuffle_deck(deck_of_cards())
 #full_deck = shuffle_deck(deck_of_cards())
 
@@ -149,3 +108,6 @@ while True or player_score < 21:
 
 
     player_score = player_inventory[0].value + player_inventory[1].value + player_card_3.value
+
+    
+    """
